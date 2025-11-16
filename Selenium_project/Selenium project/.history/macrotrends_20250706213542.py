b9dff47a0f680a1a7859b19a9325a1f8e170eea3
AmineHamzaoui <115433738+AmineHamzaoui@users.noreply.github.com>
2025-11-16 13:37:49 +0100

    Delete Selenium_project/Selenium project/.history directory

--- Selenium_project/Selenium project/.history/macrotrends_20250706213542.py
+++
@@ -1,7 +0,0 @@
-
-import undetected_chromedriver as uc
-options = uc.ChromeOptions()
-options.add_argument('--headless')  # Optional
-
-driver = uc.Chrome(options=options)
-driver.get("https://example.com")
